ce6bb12d892bcffcaf39871f76cb8e5dbf3944ce
Frank <fcfort@users.noreply.github.com>
2016-01-19 15:00:00 -0500

    Create ScreenshotDiff.java

---
+++ src/main/java/asdff/ScreenshotDiff.java
@@ -0,0 +1,26 @@
+package asdff;
+
+import java.util.Arrays;
+
+public class ScreenshotDiff {
+  private final byte[] oldData;
+  private final byte[] newData;
+  
+  public ScreenshotDiff(byte[] oldData, byte[] newData) {
+    this.oldData = oldData;
+    this.newData = newData;
+  }
+  
+  public byte[] getOldData() {
+    return oldData;
+  }
+  
+  public byte[] getNewData() {
+    return newData;
+  }
+  
+  boolean isDifferent() {
+    return !Arrays.equals(oldData, newData);
+  }
+}
+
