0793317f3c298860fa836f4e6d06c3493828a7e9
Frank Fort <phranck+github@gmail.com>
2016-02-02 20:11:11 -0500

    Deprecates unsafe gmail emailer. Fixes #16

--- src/main/java/uo/idoc/main/IdocCheckerMain.java
+++ src/main/java/uo/idoc/main/IdocCheckerMain.java
@@ -7,11 +7,11 @@ import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.google.common.collect.Lists;
 
-import uo.idoc.worker.ImageDiffWorker;
 import uo.idoc.SimpleByteDiffer;
-import uo.idoc.email.GmailUnsafeService;
+import uo.idoc.email.GmailOAuthService;
 import uo.idoc.runner.ScreenshotRunner;
 import uo.idoc.worker.ImageDiffEmailer;
+import uo.idoc.worker.ImageDiffWorker;
 import uo.idoc.worker.ImageDiffWriter;
 
 public class IdocCheckerMain {
@@ -47,7 +47,7 @@ public class IdocCheckerMain {
 
   public void run() throws IOException {
     ImageDiffWriter checker = new ImageDiffWriter(outputPath);
-    ImageDiffEmailer emailer = new ImageDiffEmailer(new GmailUnsafeService(gmailUsername, gmailPassword), recipients);
+    ImageDiffEmailer emailer = new ImageDiffEmailer(new GmailOAuthService(), recipients);
 
     List<ImageDiffWorker> workers = Lists.<ImageDiffWorker> newArrayList(checker, emailer);
     new ScreenshotRunner(imageUrl, imageHeightPx, imageWidthPx, checkIntervalSeconds, new SimpleByteDiffer(),
--- src/main/java/uo/idoc/worker/ImageDiffEmailer.java
+++ src/main/java/uo/idoc/worker/ImageDiffEmailer.java
@@ -3,16 +3,16 @@ package uo.idoc.worker;
 import java.util.List;
 
 import uo.idoc.difference.ByteArrayDifference;
-import uo.idoc.email.GmailUnsafeService;
+import uo.idoc.email.EmailService;
 
 public class ImageDiffEmailer implements ImageDiffWorker {
   
   private final String IDOC_SUBJECT = "Found IDOC!";
   
-  private final GmailUnsafeService emailer;
+  private final EmailService emailer;
   private final List<String> recipients;
  
-  public ImageDiffEmailer(GmailUnsafeService emailer, List<String> recipients) {
+  public ImageDiffEmailer(EmailService emailer, List<String> recipients) {
     this.emailer = emailer;
     this.recipients = recipients;
   }
